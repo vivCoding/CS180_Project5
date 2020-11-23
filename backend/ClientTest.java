9dd47cca7071f8ecfcfda79e107b3b08690bf939
vuser <vvvu@purdue.edu>
2020-11-23 13:15:54 -0500

    added server and client testing program. Will expand later

---
+++ backend/ClientTest.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+/**
+    * Client testing
+    *
+    * A dummie client program to ensure that
+    * server works as intended
+    *
+    * @author Team 15-3, CS 180 - Merge
+    * @version November 23, 2020
+    *
+*/
+
+public class ClientTest {
+    
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        Socket socket = new Socket("localhost", 4242);
+        System.out.println("Connected to server!");
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter writer = new PrintWriter(socket.getOutputStream());
+
+        Scanner scan = new Scanner(System.in);
+        String message;
+        do {
+            System.out.print("Send message to server: ");
+            message = scan.nextLine();
+            writer.write(message);
+            writer.println();
+            writer.flush();
+        } while (!message.equals("close server"));
+
+        socket.close();
+        writer.close();
+        reader.close();
+        scan.close();
+    }
+}
--- backend/Server.java
+++ backend/Server.java
@@ -4,7 +4,7 @@ import java.net.*;
 /**
     * Server
     *
-    * brief description of the program
+    * Handles the server creation
     *
     *
     * @author Team 15-3, CS 180 - Merge
@@ -18,5 +18,18 @@ public class Server {
         System.out.println("Waiting for clients to connect...");
         Socket socket = serverSocket.accept();
         System.out.println("Client connected!");
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter writer = new PrintWriter(socket.getOutputStream());
+
+        String message = "";
+        do {
+            message = reader.readLine();
+            System.out.println("Received from client: " + message);
+        } while (!message.equals("close server"));
+
+        serverSocket.close();
+        reader.close();
+        writer.close();
     }
 }
