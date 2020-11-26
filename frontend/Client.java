984bd2287b50fe8f3ebc61616680f6a563b114fc
Amy Curtland <fanta@DESKTOP-OT9EBEI>
2020-11-26 14:28:10 -0500

    Added server connection and client close feature
    This is currently just a simple system to enter server connection info
    and create a socket to connect to. Added a close client option to the
    primary menu and made it the only way for the Client to close without a
    crash.

--- frontend/Client.java
+++ frontend/Client.java
@@ -1,6 +1,8 @@
 ﻿package frontend;
 
 import javax.swing.*;
+import java.io.*;
+import java.net.*;
 
 import backend.Account;
 
@@ -15,11 +17,17 @@ import backend.Account;
  *  @version November 26, 2020
  */
 
-public class Client {
+public class Client { //TODO add connection to the server code, create friend and profile menus
 	private static String accountName;
 	private static String pass;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		
+		//find the server info so that the server host can be remote, or on a different port
+		String hostname = JOptionPane.showInputDialog(null, "Server Hostname", JOptionPane.QUESTION_MESSAGE);
+		int port = Integer.parseInt(JOptionPane.showInputDialog(null, "Server Port", JOptionPane.QUESTION_MESSAGE));
+		Socket socket = new Socket(hostname, port);
+		
 		boolean hasAccount = false;
 		//creates a login or account creation request to send to the server
 		String[] makeNew = {"Login", "Create new Account"};
@@ -28,11 +36,22 @@ public class Client {
 					makeNew[0]);
 			accountName = JOptionPane.showInputDialog(null, "Account Name", JOptionPane.QUESTION_MESSAGE);
 			pass = JOptionPane.showInputDialog(null, "Password", JOptionPane.QUESTION_MESSAGE);
+			
+			
+			
 			if (response.equals("Login")) {
 				//send account name and pass to login
 			
 			} else {
 				//send account name and pass and do account setup
+				//section for contact info
+				String email = JOptionPane.showInputDialog(null, "Please enter your email", JOptionPane.QUESTION_MESSAGE);
+				String phone = JOptionPane.showInputDialog(null, "Please enter your phone number", JOptionPane.QUESTION_MESSAGE);
+				
+				//section to gain all other needed account info to create the Account
+				String bio = JOptionPane.showInputDialog(null, "Please enter your bio", JOptionPane.QUESTION_MESSAGE);
+				String interests = JOptionPane.showInputDialog(null, "Please enter some of your interests", JOptionPane.QUESTION_MESSAGE);
+				
 			}
 			
 			// if connection is established, and account login is valid / valid account creation, then hasAccount becomes true.
@@ -41,19 +60,27 @@ public class Client {
 		
 		//at this point client is connected to server and logged in
 		
-		String[] menus = {"Friends", "Profile"};
+		//base menu for client, when running the client is still running
+		//once the Close option is chosen, the client closes all resources and terminates
+		String[] menus = {"Friends", "Profile", "Close Client"};
 		
 		String menuChoice = (String) JOptionPane.showInputDialog(null, "Home", "", JOptionPane.QUESTION_MESSAGE, null, menus,
 				menus[0]);
-		switch(menuChoice) {
-		case "Friends":
-			friendMenu();
-			break;
-		case "Profile": 
-			profileMenu();
-			break;
+		do {
+			switch(menuChoice) {
+			case "Friends":
+				friendMenu();
+				break;
+			case "Profile": 
+				profileMenu();
+				break;
+			case "Close Client":
+				//close all client resources here
+				socket.close();
+				return;
+			} 
 		
-		}
+		} while(true);
 
 	}
 	
